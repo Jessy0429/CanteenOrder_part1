4e8d9080afb538fc0644ad09a32fed3ea82b75de
Jessy0429 <1534138170@qq.com>
2021-10-22 17:03:55 +0800

    Update UserMain.vue

--- src/components/UserMain.vue
+++ src/components/UserMain.vue
@@ -1,10 +1,27 @@
 <template>
-  <span>今天吃点啥？</span>
+  <span style="font-size: larger; font-family: 'Microsoft JhengHei'">今天吃点啥？</span>
+<!--  <div>-->
+<!--&lt;!&ndash;    <el-select v-model="sel_Carteen" placeholder="选择食堂">&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;      <el-option&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;        v-for="item in options"&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;        :key="item.value"&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;        :label="item.label"&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;        :value="item.value">&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;      </el-option>&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;    </el-select>&ndash;&gt;-->
+<!--  </div>-->
+
 </template>
 
 <script>
 export default {
-  name: "UserMain"
+  name: "UserMain",
+  data(){
+    return{
+      // Carteens:['一食堂', '二食堂', '三食堂','四食堂'],
+      // sel_Carteen:undefined,
+    }
+  }
 }
 </script>
 
